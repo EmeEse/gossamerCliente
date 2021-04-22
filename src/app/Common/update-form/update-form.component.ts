b9ef10cdb752a49327fa4bb4fb109181da2846be
EmeEse <speronimauri@gmail.com>
2021-04-22 15:54:20 +0200

    Agregado de listado Empleado en form

--- src/app/Common/update-form/update-form.component.ts
+++ src/app/Common/update-form/update-form.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Cita } from 'src/app/Modelo/Cita';
+import { Empleado } from 'src/app/Modelo/Empleado';
 import { ServiceService } from 'src/app/Service/service.service';
 @Component({
   selector: 'app-update-form',
@@ -10,6 +11,7 @@ import { ServiceService } from 'src/app/Service/service.service';
   styleUrls: ['./update-form.component.css']
 })
 export class UpdateFormComponent implements OnInit {
+  empleados:Empleado[] | undefined;
   cita:Cita = new Cita;
   formularioCita: FormGroup;
   constructor(private service:ServiceService, private router:Router, private formBuilder: FormBuilder) {
@@ -26,9 +28,15 @@ export class UpdateFormComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.service.getEmpleados()
+    .subscribe(data=>{this.empleados=data;})
   }
   crearCita(){
     let parameter = JSON.stringify(this.formularioCita.value);
     this.service.addCita(parameter).subscribe((data)=>{alert('Cita creada con exitao'); this.router.navigate(['listarCita']);})
   }
+  listarTrabajos(){
+    
+  }
+
 }
